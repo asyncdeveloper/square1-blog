398e69a6c48329ec8119367b64cc4bd1879b9c2e
asyncdeveloper <conatct@asyncdeveloper.com>
2021-03-11 17:44:53 +0100

    Add ui for create post page

---
+++ resources/views/layouts/partials/alert.blade.php
@@ -0,0 +1,26 @@
+@if($errors->any())
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        {!! implode('', $errors->all('<div>:message</div>')) !!}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
+
+@if (session('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <p class="mb-0">{{ session('success') }}</p>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
+
+@if (session('error'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <p class="mb-0">{{ session('error') }}</p>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Create a Post</div>
+
+                    <div class="card-body">
+                        @include('layouts.partials.alert')
+                        <form method="POST" action="{{ route('posts.store') }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="title" class="col-md-4 col-form-label text-md-right">Title</label>
+                                <div class="col-md-6">
+                                    <input id="title" class="form-control" name="title" value="{{ old('title') }}" required autofocus>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="description" class="col-md-4 col-form-label text-md-right">Description</label>
+                                <div class="col-md-6">
+                                    <textarea name="description" id="description" class="form-control">{{old('description')}}</textarea>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="publication_date" class="col-md-4 col-form-label text-md-right">Publication Date</label>
+                                <div class="col-md-6">
+                                    <input name="publication_date" id="publication_date" class="form-control" type="date" >
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">Create</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
